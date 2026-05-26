0ad2076bc2616770a85ed8297e99cc29bd98e952
Renato Augusto <73561558+ragoncalves@users.noreply.github.com>
2026-05-26 17:45:53 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.calcrampa.app',
+  appName: 'CalcRampa',
+  webDir: 'dist'
+};
+
+export default config;
